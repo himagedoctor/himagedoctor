13dd604f378ebe24020b042c0074d29929bf65d3
Johann Chen <johannchen@gmail.com>
2016-12-22 11:17:15 -0800

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {HomeMenu} from '../components/HomeMenu';
+//import {Intro} from './Intro';
+//import {About} from './About';
+//import {Team} from './Team';
+import Footer from '../components/Footer';
 
-export default () => (
-  <div>Welcome to next.js!</div>
-)
+class Home extends React.Component {
+  /*
+  componentDidMount() {
+    window.onscroll=function() {
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("top-nav").classList.add('top-nav-collapse');
+      } else {
+        document.getElementById("top-nav").classList.remove('top-nav-collapse');
+      }
+    }
+  }
+  */
+  render() {
+    return (
+      <div>
+        <HomeMenu />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Home;
