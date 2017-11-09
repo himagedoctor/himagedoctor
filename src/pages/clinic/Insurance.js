9004a3fe46f0f42e2165a3bfc8630404da5f8508
Johann Chen <johannchen@gmail.com>
2017-11-09 12:07:03 -0800

    update insurance images

--- src/pages/clinic/Insurance.js
+++ src/pages/clinic/Insurance.js
@@ -1,71 +1,43 @@
 import React from 'react'
-//
-/*
-import ins1 from '../../images/insurance/ins1.jpg'
-import ins2 from '../../images/insurance/ins2.jpg'
-import ins3 from '../../images/insurance/ins3.jpg'
-import ins4 from '../../images/insurance/ins4.gif'
-import ins5 from '../../images/insurance/ins5.png'
-import ins6 from '../../images/insurance/ins6.jpg'
-import ins7 from '../../images/insurance/ins7.jpg'
-import ins8 from '../../images/insurance/ins8.jpg'
-import ins9 from '../../images/insurance/ins9.png'
-import ins10 from '../../images/insurance/ins10.jpg'
-import ins11 from '../../images/insurance/ins11.jpg'
-import ins12 from '../../images/insurance/ins12.jpg'
-import ins13 from '../../images/insurance/ins13.jpg'
-import ins14 from '../../images/insurance/ins14.jpg'
-import ins15 from '../../images/insurance/ins15.jpg'
-import ins16 from '../../images/insurance/ins16.jpg'
-import ins17 from '../../images/insurance/ins17.jpg'
-import ins18 from '../../images/insurance/ins18.jpg'
-import ins19 from '../../images/insurance/ins19.jpg'
-import ins20 from '../../images/insurance/ins20.jpg'
-import ins21 from '../../images/insurance/ins21.gif'
-import ins22 from '../../images/insurance/ins22.jpg'
-import ins23 from '../../images/insurance/ins23.jpg'
-import ins24 from '../../images/insurance/ins24.png'
-*/
 
 import axa from '../../images/insurance/axa.png'
 import erv from '../../images/insurance/erv.png'
 import medilink from '../../images/insurance/medilink.png'
+import aetna from '../../images/insurance/aetna.png'
+import allianz from '../../images/insurance/allianz.png'
+import bluecross from '../../images/insurance/bluecross.png'
+import bupa from '../../images/insurance/bupa.png'
+import covermore from '../../images/insurance/covermore.png'
+import ea from '../../images/insurance/ea.png'
+import falck from '../../images/insurance/falck.png'
+import general from '../../images/insurance/general.png'
+import geoblue from '../../images/insurance/geoblue.png'
+import henner from '../../images/insurance/henner.png'
+import hth from '../../images/insurance/hth.png'
+import img from '../../images/insurance/img.png'
+import msh from '../../images/insurance/msh.png'
+import sos from '../../images/insurance/sos.png'
+import uhg from '../../images/insurance/uhg.png'
 let images = [
-  /*
-  {original: ins1, key: 1},
-  {original: ins2, key: 2},
-  {original: ins3, key: 3},
-  {original: ins4, key: 4},
-  {original: ins5, key: 5},
-  {original: ins6, key: 6},
-  {original: ins7, key: 7},
-  {original: ins8, key: 8},
-  {original: ins9, key: 9},
-  {original: ins10, key: 10},
-  {original: ins11, key: 11},
-  {original: ins12, key: 12},
-  {original: ins13, key: 13},
-  {original: ins14, key: 14},
-  {original: ins15, key: 15},
-  {original: ins16, key: 16},
-  {original: ins17, key: 17},
-  {original: ins18, key: 18},
-  {original: ins19, key: 19},
-  {original: ins20, key: 20},
-  {original: ins21, key: 21},
-  {original: ins22, key: 22},
-  {original: ins23, key: 23},
-  {original: ins24, key: 24},
-  */
   {original: axa, key: 1},
   {original: erv, key: 2},
   {original: medilink, key: 3},
+  {original: aetna, key: 4 },
+  {original: allianz, key: 5},
+  {original: bluecross, key: 6},
+  {original: bupa, key: 7},
+  {original: covermore, key: 8},
+  {original: ea, key: 9},
+  {original: falck, key: 10},
+  {original: general, key: 11},
+  {original: geoblue, key: 12},
+  {original: henner, key: 13},
+  {original: hth, key: 14},
+  {original: img, key: 15},
+  {original: msh, key: 16},
+  {original: sos, key: 17},
+  {original: uhg, key: 18},
 ];
-/*
-for (let i = 1; i <= 6; i++) {
-  images.push({original: eval(`ins${i}`), key: i});
-}
-*/
 
 const Thumbnail = ({picture}) => (
   <div className="col-xs-6 col-md-3">
